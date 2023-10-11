09b08508b28693646efce7ca617b2c6158f49d12
milkyway <i.am.molokova@gmail.com>
2023-10-11 14:40:46 +0300

    закынчила 11.10 о 14:40

---
+++ app/src/main/java/com/example/cashluckpatrol/EnteringEmailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cashluckpatrol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EnteringEmailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entering_email)
+    }
+}
