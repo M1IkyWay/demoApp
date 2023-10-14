d33e83ec33eae575ff0be12eb5b0677726a6556c
milkyway <i.am.molokova@gmail.com>
2023-10-14 13:27:54 +0300

    закінчила 14.10 о 13:27

--- app/src/main/java/com/example/cashluckpatrol/PlayActivity.kt
+++ app/src/main/java/com/example/cashluckpatrol/PlayActivity.kt
@@ -1,11 +1,22 @@
 package com.example.cashluckpatrol
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cashluckpatrol.databinding.ActivityPlayBinding
 
 class PlayActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityPlayBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_play)
+        binding = ActivityPlayBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPlay.setOnClickListener{
+            val intent = Intent(this, ChoosingModeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
