f9684a7367bb496d7565619d3bdee6bb5b27e1a4
milkyway <i.am.molokova@gmail.com>
2023-10-15 13:51:32 +0300

    почала 15.10 о 13:51

--- app/src/main/java/com/example/cashluckpatrol/EnteringPhoneActivity.kt
+++ app/src/main/java/com/example/cashluckpatrol/EnteringPhoneActivity.kt
@@ -2,6 +2,7 @@ package com.example.cashluckpatrol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import com.example.cashluckpatrol.databinding.ActivityEnteringPhoneBinding
 import me.ibrahimsn.lib.PhoneNumberKit
 
@@ -13,15 +14,19 @@ class EnteringPhoneActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        val inputPhone = findViewById<EditText>(R.id.numberInput)
+
         val phoneNumberKit = PhoneNumberKit.Builder(this)
             .setIconEnabled(true)
             .admitCountries(listOf("tr", "ca", "de")) // List only those county formats
             .excludeCountries(listOf("tr", "ca")) // Exclude those county formats
             .build()
 
-        phoneNumberKit.attachToInput(textField, "tr")
+        phoneNumberKit.attachToInput(textField, "ua")
 // OR
         phoneNumberKit.attachToInput(textField, 1)
+
+        phoneNumberKit.setupCountryPicker(this) // Requires activity context
 
     }
 }
