4c806b5041de20cf5bd2dc508ed742d95d32909d
milkyway <i.am.molokova@gmail.com>
2023-10-15 12:44:47 +0300

    закінчила 15.10 о 12:44

--- app/src/main/java/com/example/cashluckpatrol/EnteringPhoneActivity.kt
+++ app/src/main/java/com/example/cashluckpatrol/EnteringPhoneActivity.kt
@@ -3,6 +3,7 @@ package com.example.cashluckpatrol
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cashluckpatrol.databinding.ActivityEnteringPhoneBinding
+import me.ibrahimsn.lib.PhoneNumberKit
 
 class EnteringPhoneActivity : AppCompatActivity() {
     lateinit var binding : ActivityEnteringPhoneBinding
@@ -12,7 +13,15 @@ class EnteringPhoneActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        val phoneNumberKit = PhoneNumberKit.Builder(this)
+            .setIconEnabled(true)
+            .admitCountries(listOf("tr", "ca", "de")) // List only those county formats
+            .excludeCountries(listOf("tr", "ca")) // Exclude those county formats
+            .build()
 
+        phoneNumberKit.attachToInput(textField, "tr")
+// OR
+        phoneNumberKit.attachToInput(textField, 1)
 
     }
 }
