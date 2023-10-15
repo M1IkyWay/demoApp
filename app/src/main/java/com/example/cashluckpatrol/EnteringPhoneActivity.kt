8b131a2d4ad8ddff786a57ce67355ca5ae532ada
milkyway <i.am.molokova@gmail.com>
2023-10-15 15:54:56 +0300

    закінчила 15.10 о 15:54

--- app/src/main/java/com/example/cashluckpatrol/EnteringPhoneActivity.kt
+++ app/src/main/java/com/example/cashluckpatrol/EnteringPhoneActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import com.example.cashluckpatrol.databinding.ActivityEnteringPhoneBinding
+import com.google.android.material.textfield.TextInputLayout
 import me.ibrahimsn.lib.PhoneNumberKit
 
 class EnteringPhoneActivity : AppCompatActivity() {
@@ -18,15 +19,34 @@ class EnteringPhoneActivity : AppCompatActivity() {
 
         val phoneNumberKit = PhoneNumberKit.Builder(this)
             .setIconEnabled(true)
-            .admitCountries(listOf("tr", "ca", "de")) // List only those county formats
-            .excludeCountries(listOf("tr", "ca")) // Exclude those county formats
+            .admitCountries(listOf("ua", "ca", "de")) // List only those county formats
             .build()
 
-        phoneNumberKit.attachToInput(textField, "ua")
-// OR
-        phoneNumberKit.attachToInput(textField, 1)
+
+        phoneNumberKit.attachToInput(binding.textField, "tr")
+
+        phoneNumberKit.attachToInput(binding.textField, 1)
 
         phoneNumberKit.setupCountryPicker(this) // Requires activity context
+        val exampleNumber = phoneNumberKit.getExampleNumber("ua")
+
+        val parsedNumber = phoneNumberKit.parsePhoneNumber(
+            number = "1266120000",
+            defaultRegion = "ua"
+        )
+
+        parsedNumber?.nationalNumber
+        parsedNumber?.countryCode
+        parsedNumber?.numberOfLeadingZeros
+
+        val formattedNumber = phoneNumberKit.formatPhoneNumber(
+            number = "1266120000",
+            defaultRegion = "us"
+        )
+
+        val flag = phoneNumberKit.getFlagIcon("ua")
+
+        phoneNumberKit.setupCountryPicker(this, R.layout.edittext_layout, searchEnabled = true)
 
     }
 }
