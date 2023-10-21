8982e452162b51f3f7c110879c91c41b9bedb45f
milkyway <i.am.molokova@gmail.com>
2023-10-21 20:38:59 +0300

    закінчила 21.10 о 20:38

---
+++ app/src/main/java/com/example/cashluckpatrol/FlashGame1Activity.kt
@@ -0,0 +1,11 @@
+package com.example.cashluckpatrol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FlashGame1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flash_game1)
+    }
+}
