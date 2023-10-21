3e49253f348eced9846856cfcf1d06f3b18e7e66
milkyway <i.am.molokova@gmail.com>
2023-10-22 00:56:53 +0300

    закінчила 22.10 о 00:56, бо вибухи

---
+++ app/src/main/java/com/example/cashluckpatrol/FlashGame2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.cashluckpatrol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FlashGame2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flash_game2)
+    }
+}
