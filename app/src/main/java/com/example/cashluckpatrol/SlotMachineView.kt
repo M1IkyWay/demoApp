16b81c52d6a9f0690e193519efdeebcae784a3f4
milkyway <i.am.molokova@gmail.com>
2023-10-23 11:40:31 +0300

    закінчила 23.10 о 11:40

--- app/src/main/java/com/example/cashluckpatrol/SlotMachineView.kt
+++ app/src/main/java/com/example/cashluckpatrol/SlotMachineView.kt
@@ -1,31 +1,47 @@
 package com.example.cashluckpatrol
 
 import android.content.Context
+import android.graphics.Canvas
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.widget.GridLayout
 import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import com.example.cashluckpatrol.databinding.SlotImageScrollBinding
 
-class SlotMachineView private constructor (context : Context, attributeSet: AttributeSet) : GridLayout(context) {
- public constructor(context : Context, attributeSet: AttributeSet, rawsNumb : Int, columnNumb : Int,
-     images : List<ImageView>) : this(context, attributeSet) {
+class SlotMachineView (context : Context) : LinearLayout (context) {
+
+//    public constructor(
+//        context: Context, attributeSet: AttributeSet, rawsNumb: Int, columnNumb: Int,
+//        images: List<ImageView>
+//    ) : this(context) {}
+    val image1 = ContextCompat.getDrawable(context, R.drawable.hot_im1)
+    val image2 = ContextCompat.getDrawable(context, R.drawable.hot_im2)
+    val image3 = ContextCompat.getDrawable(context, R.drawable.hot_im3)
+    val image4 = ContextCompat.getDrawable(context, R.drawable.hot_im4)
+    val image5 = ContextCompat.getDrawable(context, R.drawable.hot_im5)
 
 
+    var imagesList = mutableListOf (image1, image2, image3, image4, image5)
 
-     }
+        init {
+            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            inflater.inflate(R.layout.slot_image_scroll, this, true)
+            val binding = SlotImageScrollBinding.inflate(inflater)
 
-    init {
+        }
 
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
 
 
 
 
     }
 
-    override fun setColumnCount(columnCount: Int) {
-        super.setColumnCount(columnCount)
+
     }
 
-    override fun setRowCount(rowCount: Int) {
-        super.setRowCount(rowCount)
-    }
-}
+
+
